32ed159d301f2f2aed694540c0ea5ccdb55fd25c
somyungsub <gkdldy5@naver.com>
2018-04-25 19:20:35 +0900

    exam3 추가 (3일차)

---
+++ src/exam3/ThreadEx24_1.java
@@ -0,0 +1,36 @@
+package exam3;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ThreadEx24_1 {
+	public static void main(String[] args) {
+		ArrayBlockingQueue<String> pool = new ArrayBlockingQueue<>(5);
+		pool.add("one"); 
+		pool.add("two"); 
+		pool.add("three");
+		pool.add("four");
+		pool.add("five");
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					String str = pool.take();
+					System.out.println("Ï∑®Îìù(" + Thread.currentThread().getName() + ") : " + str);
+
+					Thread.sleep(3000);
+
+					System.out.println("?ï¥?†ú(" + Thread.currentThread().getName() + ") : " + str);
+					pool.add(str);
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+
+		for (int i = 0; i < 10; i++) {
+			Thread thread = new Thread(r);
+			thread.start();
+		}
+	}
+}
